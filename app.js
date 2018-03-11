372b99ff80ba24f1f558a358cde8e38273b44852
mikah <anhminhhoang13@gmail.com>
2018-03-10 21:22:00 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+// Routing
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, 'index.html'));
+});
+// Starts the server.
+
+var players = {};
+
+io.on('connection', function(socket) {
+
+    socket.on('disconnect', function() {
+        delete players[socket.id];
+        let msg = `Player ${socket.id} has left`;
+        io.emit('disconnect',msg);
+    });
+
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    });
+
+});
+
+server.listen(5000, function() {
+    console.log('Starting server on port 5000');
+});
